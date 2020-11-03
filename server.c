613826c94b5ccb1320156f6c0623ffa09555c228
MSS <875396916@qq.com>
2020-11-03 10:14:00 +0800

    [server]

---
+++ server.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define SERV_PORT    5001
+#define SERV_IP_ADDR "192.168.136.100"
+
+#define BACK_LOG 5
+#define BUFFER_SIZE 128
+
+int main(int argc, const char *argv[])
+{
+	int fd;
+
+	return 0;
+}
+
